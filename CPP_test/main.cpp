70b00b867dccdbc0725c2d4ac3c9a702e6516074
Akshay <akshayyadav7613@gmail.com>
2019-10-02 13:49:55 +0530

    c++ program

--- CPP_test/main.cpp
+++ CPP_test/main.cpp
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include<iostream>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
-}
+int main(){
+    
+    std::cout <<"hellow see....\n";
+    return 0;
+    
+    }
